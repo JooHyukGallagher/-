4c9d2b41a24c1ef4f29f5d67f896d3f1ea6eeb5c
joohyuk <vfrvfr4207@gmail.com>
2020-04-07 20:06:01 +0900

    solved

---
+++ hackerrank/Strings/The Love-Letter Mystery.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the theLoveLetterMystery function below.
+    static int theLoveLetterMystery(String s) {
+        int ans = 0;
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+        for(int i=0; i<n/2; i++){
+            if(arr[i] != arr[n-1-i]){
+                ans += Math.abs(arr[i] - arr[n-1-i]);
+            }
+        }
+        return ans;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = scanner.nextLine();
+
+            int result = theLoveLetterMystery(s);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
