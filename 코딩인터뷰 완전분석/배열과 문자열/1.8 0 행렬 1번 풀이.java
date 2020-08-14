856320e3bdaf73887e53ee6958c2a4443b72d771
weekbelt <vfrvfr4207@gmail.com>
2020-08-15 00:01:33 +0900

    solve

---
+++ 코딩인터뷰 완전분석/배열과 문자열/1.8 0 행렬 1번 풀이.java
@@ -0,0 +1,62 @@
+package algorithms.codingInterview.str;
+
+public class exam08_01 {
+    static void setZeros(int[][] matrix) {
+        boolean[] row = new boolean[matrix.length];
+        boolean[] column = new boolean[matrix[0].length];
+        // 값이 0인 행과 열의 인덱스를 저장한다.
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    column[j] = true;
+                }
+            }
+        }
+
+        // 행의 원소를 전부 0으로 바꾼다.
+        for (int i = 0; i < row.length; i++) {
+            if (row[i]) {
+                nullifyRow(matrix, i);
+            }
+        }
+
+        // 열의 원소를 전부 0으로 바꾼다.
+        for (int j = 0; j < column.length; j++) {
+            if (column[j]) {
+                nullifyColumn(matrix, j);
+            }
+        }
+    }
+
+    private static void nullifyRow(int[][] matrix, int row) {
+        for (int j = 0; j < matrix[0].length; j++) {
+            matrix[row][j] = 0;
+        }
+    }
+
+    private static void nullifyColumn(int[][] matrix, int column) {
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i][column] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 1, 2, 2, 2, 1},
+                {3, 1, 2, 0, 2, 1},
+                {5, 1, 2, 1, 2, 1},
+                {1, 1, 2, 1, 2, 1},
+        };
+
+        setZeros(matrix);
+
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
