f1a6055f7b777cd6ddd93761a2b13dfbe72ea63e
weekbelt <vfrvfr4207@gmail.com>
2020-10-05 05:55:33 +0900

    solve

---
+++ 프로그래머스/Python/level2/점프와 순간 이동.py
@@ -0,0 +1,18 @@
+def solution(n):
+    ans = 0
+
+    while n != 0:
+        if n % 2 == 0:
+            n /= 2
+        else:
+            n -= 1
+            ans += 1
+    return ans
+
+
+n = 5
+print(solution(n))
+n = 6
+print(solution(n))
+n = 5000
+print(solution(n))
